bcd9474bb807521bb1f38a22a16d3bcfe5535722
jus95003 <jus95003@byui.edu>
2023-10-12 17:49:57 -0700

    fraction program code

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        FractionWriter test1 = new FractionWriter();
+        Console.WriteLine(test1.GetFractionString());
+        Console.WriteLine(test1.GetDecimalValue());
+
+        FractionWriter test2 = new FractionWriter(5);
+        Console.WriteLine(test2.GetFractionString());
+        Console.WriteLine(test2.GetDecimalValue());
+
+        FractionWriter test3 = new FractionWriter(3, 4);
+        Console.WriteLine(test3.GetFractionString());
+        Console.WriteLine(test3.GetDecimalValue());
+
+        FractionWriter test4 = new FractionWriter(1, 3);
+        Console.WriteLine(test4.GetFractionString());
+        Console.WriteLine(test4.GetDecimalValue());
     }
 }
---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,58 @@
+public class FractionWriter
+{
+    private int _numerator;
+    private int _denominator;
+
+    public FractionWriter()
+    {
+        _numerator = 1;
+
+        _denominator = 1;
+    }
+
+    public FractionWriter(int numerator)
+    {
+        _numerator = numerator;
+
+        _denominator = 1;
+    }
+
+    public FractionWriter(int numerator, int denominator)
+    {
+        _numerator = numerator;
+
+        _denominator = denominator;
+    }
+
+    public void GetNumerator()
+    {
+        Console.WriteLine(_numerator);
+    }
+
+    public void SetNumerator(int numerator)
+    {
+        _numerator = numerator;
+    }
+
+    public void GetDenominator()
+    {
+        Console.WriteLine(_denominator);
+    }
+
+    public void SetDenominator(int denominator)
+    {
+        _denominator = denominator;
+    }
+
+    public string GetFractionString()
+    {
+        string _fraction = _numerator + "/" + _denominator;
+        return _fraction;
+    }
+
+    public double GetDecimalValue()
+    {
+        double _decimal = (double)_numerator / _denominator;
+        return _decimal;
+    }
+}
