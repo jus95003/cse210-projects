0b6cdecfc360fa96c262e4a9ef7f66d1b3942782
jus95003 <jus95003@byui.edu>
2023-11-06 21:00:48 -0700

    Learning Activity 5 code update

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapeList = new List<Shape>();
+
+        Square square = new Square("Blue", 8);
+        shapeList.Add(square);
+
+        Rectangle rectangle = new Rectangle("Green", 10, 5);
+        shapeList.Add(rectangle);
+
+        Circle circle = new Circle("Orange", 12);
+        shapeList.Add(circle);
+
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea()}.");
+        }
     }
 }
